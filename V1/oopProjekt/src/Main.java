ba3ae5457c4cac9fd0b18a78e345c6d54d41c4e9
Brian <dragoneisbaer07@gmail.com>
2026-04-13 13:06:40 +0200

    init

---
+++ V1/oopProjekt/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Ein einfaches Java-Programm, das "Hallo Welt!" auf der Konsole ausgibt.
+ * Es dient als Einstiegspunkt für Java-Programmierung.
+ * @author brian
+ */
+public class Main {
+
+    /**
+     * Gibt Hallo Welt! auf der Konsole aus.
+     * @param args keine Nutzung vorgesehen
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt!");
+    }
+}
